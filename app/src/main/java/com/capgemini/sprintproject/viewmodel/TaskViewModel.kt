603c7a925cac04c93bb9a80345af64301f600053
11guptakhushi <71972602+11guptakhushi@users.noreply.github.com>
2024-02-08 09:40:14 +0530

    viewModel commit

---
+++ app/src/main/java/com/capgemini/sprintproject/viewmodel/TaskViewModel.kt
@@ -0,0 +1,51 @@
+package com.capgemini.tasktracker.viewmodel
+
+import android.app.Application
+import android.app.DownloadManager.Query
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.capgemini.tasktracker.model.Task
+import com.capgemini.tasktracker.model.TaskRepository
+import com.capgemini.tasktracker.model.User
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class TaskViewModel(application: Application): AndroidViewModel(application) {
+    //owns repository
+    private val repo =TaskRepository(application)
+    var taskList: MutableLiveData<List<Task>> = repo.allTasks as MutableLiveData<List<Task>>
+
+    fun insertUser(user: User){
+        CoroutineScope(Dispatchers.IO).launch{
+            repo.insertUser(user)
+        }
+    }
+    fun updateUser(user: User){
+        CoroutineScope(Dispatchers.IO).launch{
+            repo.updateUser(user)
+        }
+    }
+    fun insertTask(task: Task){
+        CoroutineScope(Dispatchers.IO).launch{
+            repo.insertTask(task)
+        }
+    }
+    fun updateTask(task: Task){
+        CoroutineScope(Dispatchers.IO).launch{
+            repo.updateTask(task)
+        }
+    }
+    fun deleteTask(task: Task){
+        CoroutineScope(Dispatchers.IO).launch{
+            repo.deleteTask(task)
+        }
+    }
+    fun searchTask(query: String): LiveData<List<Task>>{
+        return repo.searchTask(query)
+    }
+    fun getAllTasks(): MutableLiveData<List<Task>>{
+        return taskList
+    }
+}
